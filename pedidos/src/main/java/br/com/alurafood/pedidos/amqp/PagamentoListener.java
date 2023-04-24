cdee4369a954d5433f3eac68ba098fe29ecb55a0
Henrique Ferreira Silva <henrique_silva_ferreira@hotmail.com>
2023-04-24 09:45:10 -0300

    inital commit

---
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -0,0 +1,24 @@
+package br.com.alurafood.pedidos.amqp;
+
+import br.com.alurafood.pedidos.dto.PagamentoDto;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagamentoListener {
+
+    @RabbitListener(queues = "pagamentos.detalhes-pedido")
+    public void recebeMensagem(PagamentoDto pagamento) {
+        String mensagem = """
+                Dados do pagamento: %s
+                Número do pedido: %s
+                Valor R$: %s
+                Status: %s
+                """.formatted(pagamento.getId(),
+                pagamento.getPedidoId(),
+                pagamento.getValor(),
+                pagamento.getStatus());
+
+        System.out.println("Recebi a mensagem " + mensagem);
+    }
+}
---
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PedidoAMQPConfiguration.java
@@ -0,0 +1,57 @@
+package br.com.alurafood.pedidos.amqp;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PedidoAMQPConfiguration {
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue filaDetalhesPedido() {
+        return QueueBuilder
+                .nonDurable("pagamentos.detalhes-pedido")
+                .build();
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return ExchangeBuilder
+                .fanoutExchange("pagamentos.ex")
+                .build();
+    }
+
+    @Bean
+    public Binding bindPagamentoPedido(FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(filaDetalhesPedido())
+                .to(fanoutExchange);
+    }
+
+    @Bean
+    public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+}
